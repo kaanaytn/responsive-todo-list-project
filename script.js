4d42e8a3726ec0c8b1a0c9a844f538abc1a4fb20
Kaan Ayten <kaanayten@hotmail.com>
2024-01-03 20:11:17 +0300

    task list item added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
+
 const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
 const btnAddNewTask = document.querySelector('#btnAddNewTask');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
-const TaskList = document.querySelector('task-list');
+const TaskList = document.querySelector('#task-list');
 
 eventListeners();
 
@@ -11,5 +13,27 @@ function eventListeners(){
 }
 
 function addNewItem(e){
-    console.log("submit");
+
+    if(input.value === ""){
+        alert("Add new item!")
+        console.log("submit");
+    }
+
+
+    const li = document.createElement("li");
+    li.className = "list-group-item list-group-item-secondary";
+    li.appendChild(document.createTextNode("input.value"));
+    
+
+    const a = document.createElement("a");
+    a.classList = "delete-item float-right";
+    a.setAttribute("href","#");
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+    li.appendChild(a);
+
+    TaskList.appendChild(li);
+
+
+    e.preventDefault();
 }
